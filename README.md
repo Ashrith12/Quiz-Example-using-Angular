0a7b74c2b6a76ae7aef846a91cc1c6bfe5591392
Ashrith12 <88572369+Ashrith12@users.noreply.github.com>
2023-07-17 09:52:42 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Quiz-Example-using-Angular
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-5nlugq)
---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import './polyfills';
+
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+declare global {
+  interface Window {
+    ngRef: any;
+  }
+}
+
+// ...
+window.ngRef;
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then((ref) => {
+    // Ensure Angular destroys itself on hot reloads.
+    if (window['ngRef']) {
+      window['ngRef'].destroy();
+    }
+    window['ngRef'] = ref;
+
+    // Otherwise, log the boot error
+  })
+  .catch((err) => console.error(err));
